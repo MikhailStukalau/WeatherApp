8e408d2bd184293d673b9a7b253da7cc4ab5d832
muxar <1>
2022-10-19 20:12:18 +0300

    start files

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,39 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
 
 def index(request):
-    return render(request, 'index.html')
+    appid = '937718b1123af2e9f2f0c69f567d1f06'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    all_cities = []
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+        if city_info not in all_cities:
+            all_cities.append(city_info)
+        else:
+            form = CityForm()
+
+    context = {
+        'all_info': all_cities,
+        'form': form,
+    }
+    return render(request, 'index.html', context)
+
